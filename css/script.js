9a10d79c5a16c1b17b12171ba0cddccb55ca0c0b
Xime <ximeefalcon19@gmail.com>
2024-07-01 18:25:32 -0300

    mi primer commit

---
+++ css/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    document.getElementById("compra").addEventListener('click', function(){
+        window.open("https://wa.me/2494590553",'_blank');
+    })
+})
+
